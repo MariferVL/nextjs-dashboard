ae7ac892a150c02e3828396b595ac13b092d8f2c
Marifer VL <mfvl.1186@gmail.com>
2024-03-03 19:12:36 -0300

    Change navigation views style

--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -1,26 +1,21 @@
 import Link from 'next/link';
 import NavLinks from '@/app/ui/dashboard/nav-links';
-import AcmeLogo from '@/app/ui/andes-logo';
 import { PowerIcon } from '@heroicons/react/24/outline';
+import AndesLogo from '@/app/ui/andes-logo';
 
 export default function SideNav() {
   return (
-    <div className="flex h-full flex-col px-3 py-4 md:px-2">
-      <Link
-        className="mb-2 flex h-20 items-end justify-start rounded-md bg-blue-600 p-4 md:h-40"
-        href="/"
-      >
-        <div className="w-32 text-white md:w-40">
-          <AcmeLogo />
-        </div>
-      </Link>
-      <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
+    <div className="flex h-full flex-col px-3 py-4 md:px-2 bg-gradient-to-b from-gray-900 to-gray-800">
+      <div className="flex h-20 my-8 items-center justify-center bg-gray-800 rounded-t-lg text-white">
+        <AndesLogo />
+      </div>
+      <div className="flex-grow mt-8 flex-col justify-between">
         <NavLinks />
-        <div className="hidden h-auto w-full grow rounded-md bg-gray-50 md:block"></div>
+        <div className="h-auto w-full mb-8"></div>
         <form>
-          <button className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
+          <button className="flex h-[48px] w-full items-center justify-center gap-2 rounded-md bg-yellow-400 text-gray-900 font-semibold py-3 px-6 hover:bg-yellow-600 transition duration-300">
+            <span>Log out</span>
             <PowerIcon className="w-6" />
-            <div className="hidden md:block">Sign Out</div>
           </button>
         </form>
       </div>
